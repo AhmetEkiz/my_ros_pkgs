e591a68a5cb6a5fe50d41fc4e887d1de400b38e0
Ahmet Ekiz <ekizoahmet9@gmail.com>
2023-05-17 12:10:59 +0300

    show_pose_topic

---
+++ src/show_pose_topic.cpp
@@ -0,0 +1,24 @@
+
+#include <ros/ros.h>
+#include <std_msgs/Int32.h>
+#include <turtlesim/Pose.h>
+#include <iostream> 
+
+// turtlesim::PoseConstPtr g_pose;
+
+void poseCallback(const turtlesim::PoseConstPtr& pose)
+{
+	// g_pose = pose;
+	ROS_INFO("Received pose:\n x:%f\n y:%f\n angular:%f\n linear:%f\n theta:%f", pose->x, pose->y, pose->angular_velocity, pose->linear_velocity, pose->theta);
+}
+
+int main(int argc, char **argv) {
+	
+	ros::init(argc, argv, "show_pose_topic");
+	ros::NodeHandle nh;
+	// subscribe
+	ros::Subscriber pose_sub = nh.subscribe("turtle1/pose", 1, poseCallback);
+	ros::spin(); 
+
+	return 0;
+}

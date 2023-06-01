cc4b1851beef7cc245f930387f312d9646eab5ff
Ahmet Ekiz <ekizoahmet9@gmail.com>
2023-06-01 15:00:04 +0300

    simple_navigation_goals_list fixed

--- simple_navigation_goals/src/simple_navigation_goals_list.cpp
+++ simple_navigation_goals/src/simple_navigation_goals_list.cpp
@@ -1,7 +1,7 @@
 // base code from:
 // https://github.com/ros-planning/navigation_tutorials/blob/indigo-devel/simple_navigation_goals_tutorial/src/simple_navigation_goals.cpp
 
-// send turtlebot to the given list of coordinates in order
+// send turtlebot3 to the given list of coordinates in order
 
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
@@ -22,25 +22,19 @@ void spinThread()
 int main(int argc, char **argv)
 {
 	// // coordinates to go x,y,yaw
-	// float coords[3][3] = {{6.3123, -4.4526, -50}, \
+	float coords[3][3] = {{6.3123, -4.4526, -50}, \
+                  {0.8100, 1.6451, 90}, \
+                  {-6.287, -2.896, -50}}; 
+	// float coords[3][3] = {{-6.287, -2.896, -50}, \
     //               {0.8100, 1.6451, 90}, \
-    //               {-6.9616, -3.1483, -50}}; 
+	// 			  {6.3123, -4.4526, -50}}; 
 	
-	float coords[3][3] = {{4.8, -5.7, -50}, \
-						  {-0.4, 0.9, 90}, \
-						  {-7.75, -4.6, 50}}; 
-
-	// float coords[3][3] = {{9.1, -5.45, -50}, \
-    //               		  {0.8100, 1.6451, 90}, \
-    //               		  {-6.9616, -3.1483, -50}}; 
-
-	// init with first position
-	float x=-4.3, y=0.25, yaw;   // to store values  
+	float x, y, yaw;   // to store positions  
 
 
 	ros::init(argc, argv, "simple_navigation_goals"); // created new node
 
-	ros::NodeHandle n; // ROS node object to communicat with ROS
+	ros::NodeHandle n;  // ROS node object to communicat with ROS
 
 	// use threads from boost - boot kutuphanesini kullanarak thread olusturacagiz
 	boost::thread spin_thread = boost::thread(boost::bind(&spinThread));
@@ -55,46 +49,38 @@ int main(int argc, char **argv)
 
 	// message type 
 	move_base_msgs::MoveBaseGoal goal;
-	
 
 
-	// TODO: read position from topic
-	// move_base_msgs::MoveBaseGoal goal_prev;    // previous coordinates of vehicle
+	while(true){
+		for(int i=0; i<3; i++){
 
+			x = coords[i][0];
+			y = coords[i][1];
+			yaw = (coords[i][2]*(M_PI/180));    // convert radian
 
-	// calculate radian
+			ROS_INFO("\nSending: \nx: %f\ny: %f, \nyaw:%f",x,y,yaw);
 
-	for(int i=0; i<3; i++){
+			// we'll send a goal to the robot to move 2 meters forward. [Select: base_link]
+			// we selected map as a origin
+			goal.target_pose.header.frame_id = "map";
+			goal.target_pose.header.stamp = ros::Time::now();
 
-		x = coords[i][0] - x;
-		y = coords[i][1] - y;
-		yaw = (coords[i][2]*(M_PI/180));    // convert radian
+			goal.target_pose.pose.position.x = x;
+			goal.target_pose.pose.position.y = y;
 
-		ROS_INFO("\nSending: \nx: %f\ny: %f, \nyaw:%f",x,y,yaw);
+			// this is not drone, so we need to pass just yaw orientation
+			goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(yaw);
 
-		// we'll send a goal to the robot to move 2 meters forward
-		goal.target_pose.header.frame_id = "base_link";
-		goal.target_pose.header.stamp = ros::Time::now();
+			ROS_INFO("Sending goal");
+			ac.sendGoal(goal);
 
-		goal.target_pose.pose.position.x = x;
-		goal.target_pose.pose.position.y = y;
+			ac.waitForResult();
 
-		ROS_INFO("\nPOSE: \nx: %f\ny: %f",goal.target_pose.pose.position.x, \
-													goal.target_pose.pose.position.y);
-
-		// this is not drone, so we need to pass just yaw orientation
-		goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(yaw);
-
-		ROS_INFO("Sending goal");
-		ac.sendGoal(goal);
-
-		ac.waitForResult();
-
-		if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-			ROS_INFO("Hooray, the base moved 2 meters forward");
-		else
-			ROS_INFO("The base failed to move forward 2 meters for some reason");
+			if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+				ROS_INFO("Hooray, Goal Reached Successfully");
+			else
+				ROS_INFO("The bot failed to move to the goal for some reason");
+		}
 	}
-
 	return 0;
 }
